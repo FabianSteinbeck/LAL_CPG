266479575fa05a29548b34cc39b322809904d2e6
FabianSteinbeck <f.steinbeck@sussex.ac.uk>
2021-02-02 15:59:59 +0000

    Small updates

--- Neuronator.py
+++ Neuronator.py
@@ -1,12 +1,23 @@
 import numpy as np
-
-# These functions setup the SNN and compute it
-# builder: defines the neuron parameters
-# computation: calculates the parameter changes within the timestep
-# NetworkStepper: loops through the network
-# weighting: defines the weights
+'''
+These functions setup the SNN and compute it
+- builder: defines the neuron parameters
+- computation: calculates the parameter changes within the timestep
+- NetworkStepper: loops through the network
+- weighting: defines the weights
+- TI: muscle neuron for turning
+- TII: muscle neuron for turning
+- TIIS: muscle neuron for speed
+'''
 
 def builder(preallocation) :
+
+    '''
+    builder sets up the network's neuron characteristics
+    :param preallocation = how many timesteps
+    :return: N = all neuron parameters
+    :return: NeuronIndices = the indices of the different neurons
+    '''
 
     NeuronIndices = tuple()
     NeuronIndices[0] = [1,2] # input neurons
@@ -16,81 +27,83 @@ def builder(preallocation) :
     NeuronIndices[4] = [9,10] # Adaptation
     NeuronIndices[5] = [11,12] # Inhibition
 
-    NP = dict()
+    N = dict()
     # Neuron Parameters
     # Membrane capacitance C_m
-    NP['C_m'] = (1e-9, 0.5e-9, 0.5e-9, 0.5e-9, 0.5e-9, 0.5e-9) #[Farad]the smaller, the stronger excitability
+    N['C_m'] = (1e-9, 0.5e-9, 0.5e-9, 0.5e-9, 0.5e-9, 0.5e-9) #[Farad]the smaller, the stronger excitability
     # Resting potential
-    NP['V_rest'] = (-0.06, -0.06, -0.06, -0.06, -0.06, -0.06) # [V]
+    N['V_rest'] = (-0.06, -0.06, -0.06, -0.06, -0.06, -0.06) # [V]
     # Cell potential V
-    NP['V'] = np.zeros(12, preallocation) # [V]
-    NP['V'][:,0] = NP['V_rest'][0] # Initial potential
+    N['V'] = np.zeros(12, preallocation) # [V]
+    N['V'][:,0] = N['V_rest'][0] # Initial potential
     # Threshold potential V_th
-    NP['V_th'] = (-0.05, -0.05, -0.05, -0.05, -0.05, -0.05) # [V]
+    N['V_th'] = (-0.05, -0.05, -0.05, -0.05, -0.05, -0.05) # [V]
     # Hyperpolarization potential V_hyper
-    NP['V_hyper'] = (-0.065, -0.065, -0.065, -0.065, -0.065, -0.065) # [V]
+    N['V_hyper'] = (-0.065, -0.065, -0.065, -0.065, -0.065, -0.065) # [V]
     # Spike potential V_spike
-    NP['V_spike'] = (0.02, 0.02, 0.02, 0.02, 0.02, 0.02) # [V]
+    N['V_spike'] = (0.02, 0.02, 0.02, 0.02, 0.02, 0.02) # [V]
     # Injection current parameters - ------------------------------------------
     # Synaptic conductance G_injection
-    NP['G_injection'] = (1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9) # [Siemens] the lower, the lower excitability
+    N['G_injection'] = (1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9, 1.75e-9) # [Siemens] the lower, the lower excitability
     # Excitatory synaptic battery
-    NP['V_injection_Ex'] = (0, 0, 0, 0, 0, 0) # [V]
+    N['V_injection_Ex'] = (0, 0, 0, 0, 0, 0) # [V]
     # Activation of synaptic conductance of the cell synapse for excitation
-    NP['A_injection_Ex'] = np.zeros(12, preallocation)
+    N['A_injection_Ex'] = np.zeros(12, preallocation)
     # Injection current excitatory
-    NP['I_injection_Ex'] = np.zeros(12, preallocation) # [A]
+    N['I_injection_Ex'] = np.zeros(12, preallocation) # [A]
     # Time constant for excitatory activation of synaptic conductance
-    NP['Tau_injection_Ex'] = (0.02, 0.02, 0.02, 0.02, 0.02, 0.02) # [s]
+    N['Tau_injection_Ex'] = (0.02, 0.02, 0.02, 0.02, 0.02, 0.02) # [s]
     # Inhibitory synaptic battery
-    NP['V_injection_In'] = (-0.08, -0.08, -0.08, -0.08, -0.08, -0.08) # [V]
+    N['V_injection_In'] = (-0.08, -0.08, -0.08, -0.08, -0.08, -0.08) # [V]
     # Activation of synaptic conductance of the cell synapse for inhibition
-    NP['A_injection_In'] = np.zeros(12, preallocation)
+    N['A_injection_In'] = np.zeros(12, preallocation)
     # Time constant for inhibitory activation of synaptic conductance
-    NP['Tau_injection_In'] = (0.03, 0.03, 0.03, 0.03, 0.03, 0.03) # [s]
+    N['Tau_injection_In'] = (0.03, 0.03, 0.03, 0.03, 0.03, 0.03) # [s]
     # Injection current inhibitory
-    NP['I_injection_In'] = np.zeros(12, preallocation) # [A]
+    N['I_injection_In'] = np.zeros(12, preallocation) # [A]
     # Spontaneous firing rate activation
-    NP['A_spontaneous'] = (0, 0.07, 0.0315, 0.06, 0, 0)
+    N['A_spontaneous'] = (0, 0.07, 0.0315, 0.06, 0, 0)
     # Synaptic adaptation parameters - ----------------------------------------
     # synaptic adaptation conductance change per activation
-    NP['G_adaptation'] = (0, 0, 0, 0, X, 0) # [Siemens]
+    N['G_adaptation'] = (0, 0, 0, 0, X, 0) # [Siemens] X is place holder
     # adaptation conductance step
-    NP['G_adaptation_Step'] = (0, 0, 0, 0, X, 0)
+    N['G_adaptation_Step'] = (0, 0, 0, 0, X, 0)
     # spike rate adaptation activation
-    NP['A_adaptation'] = np.zeros(12, preallocation)
+    N['A_adaptation'] = np.zeros(12, preallocation)
     # spike rate adaptation activation power
-    NP['A_adaptation_P'] = (1, 1, 1, 1, X, 1)
+    N['A_adaptation_P'] = (1, 1, 1, 1, X, 1)
     # adaptation battery for hyperpolarization
-    NP['V_adaptation'] = (0, 0, 0, 0, -0.07, 0) # [V]
+    N['V_adaptation'] = (0, 0, 0, 0, -0.07, 0) # [V]
     # spike rate adaptation time constant
-    NP['Tau_adaptation'] = (9999999999, 9999999999, 9999999999, 9999999999, X, 9999999999) # [s], huge values for excitatory neurons which don't adapt
+    N['Tau_adaptation'] = (9999999999, 9999999999, 9999999999, 9999999999, X, 9999999999) # [s], huge values for excitatory neurons which don't adapt
     # synaptic input current
-    NP['I_adaptation'] = np.zeros(12, preallocation)
+    N['I_adaptation'] = np.zeros(12, preallocation)
     # Leak parameters - -------------------------------------------------------
     # Leak current
-    NP['I_leak'] = np.zeros(12, preallocation) # [A]
+    N['I_leak'] = np.zeros(12, preallocation) # [A]
     # Leak conductance
-    NP['G_leak'] = (5e-9, 5e-9, 5e-9, 5e-9, 5e-9, 5e-9) # [Siemens]
+    N['G_leak'] = (5e-9, 5e-9, 5e-9, 5e-9, 5e-9, 5e-9) # [Siemens]
     # spike events
-    NP['spike'] = np.zeros(12, preallocation) # [Binary]
+    N['spike'] = np.zeros(12, preallocation) # [Binary]
 
-    return NP, NeuronIndices
+    return N, NeuronIndices
 
 # -----------------------------------------------------------------------------------------------------
 def computation(t,input,N,dt,population,neuronIndex) :
-    # Parameters: Inputs
-    # t = timestep
-    # input = current spikes of the whole network
-    # Neuron = all neuron parameters
-    # dt = time resolution
-    # population = which of the network population is the current calculation
+    '''
+    computation calculates the new voltage state of a neuron, considering Ex/In inputs and adaptation
+    :param t = timestep
+    :param input = current spikes of the whole network calculated with weights
+    :param N = all neuron parameters
+    :param dt = time resolution
+    :param population = which of the network population is the current calculation
     # --> difference between inhibitory/excitatory
-    # neuronIndex = which neuron of the whole network
-
+    :param neuronIndex = which neuron of the whole network
+    :return N = see above
+    '''
     # first test if the neuron spikes now and change Activations accordingly
-    if N['spike'][neuronIndex,t] == 1: #% did this neuron spike last time?
-        # do not take external inputs
+    if N['spike'][neuronIndex,t] == 1: ''' did this neuron spike last time?
+        do not take external inputs'''
     else: # it didn't, take external inputs
         # set excitatory injection activation
         N['A_injection_Ex'][neuronIndex,t] = \
@@ -142,7 +155,7 @@ def computation(t,input,N,dt,population,neuronIndex) :
             + N['I_injection_In'][neuronIndex,t]\
             + N['I_adaptation'][neuronIndex,t])
         # add noise
-        dV = dV + dV*0.275*rand(1);
+        dV = dV + dV*0.275*np.random.rand(1)
         N['V'][neuronIndex,t+1] = N['V'][neuronIndex,t] + dV
 
     # synaptic activation variable A: decay
@@ -182,42 +195,53 @@ def computation(t,input,N,dt,population,neuronIndex) :
 # ----------------------------------------------------------------------------------------------------
 def NetworkStepper(N,N_Indices,t,dt,f,W):
 
+'''
+:param N = Neuron parameters
+:param N_Indices = Neuron indices
+:param t = current time step
+:param dt = step size in [s]
+:param f = familiarity
+:param W = WeightMatrix
+:return N = see above
+'''
+
 # neuron calculations
 for i in np.size(N_Indices,1) :# loop through all populations
     if i == 0 :# 1st population
         for ii in i:N_Indices(i+1) :# each ii depicts one neuron of the population
-            N = NeuuCom(t,W(:,ii).*f,N,dt,i,ii) # input is familiarity
+            N = computation(t,W(:,ii).*f,N,dt,i,ii) # input is familiarity
 
     elif i == 1 :# 2nd population
         for ii in (N_Indices(i-1)+1):N_Indices(i):
-            N = NeuuCom(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
+            N = computation(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
 
     elif i == 2 :# 3rd population
         for ii in (N_Indices(i-1)+1):N_Indices(i):
-            N = NeuuCom(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
+            N = computation(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
 
     elif i == 3 :# 4th population
         for ii in (N_Indices(i-1)+1):N_Indices(i):
-            N = NeuuCom(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
+            N = computation(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
 
     elif i == 4 :# 5th population
         for ii in (N_Indices(i-1)+1):N_Indices(i):
-            N = NeuuCom(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
+            N = computation(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
 
     elif i == 5 :# 6th population
         for ii in (N_Indices(i-1)+1):N_Indices(i):
-            N = NeuuCom(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
+            N = computation(t,W(:,ii).*N.spike(:,t),N,dt,i,ii)
 
     return N
 
 # ----------------------------------------------------------------------------------------------------
-def Weighting(C):
-
-    # each weight represents the strength of connection to that neuron
-    # columns: each neuron of the populations
-    # rows: connections from the other neurons
-    # negative weights depict inhibitory connections
-
+def Weighting():
+    '''
+    each weight represents the strength of connection to that neuron
+    columns: each neuron of the populations
+    rows: connections from the other neurons
+    negative weights depict inhibitory connections
+    :return Weights: Matrix, where each Column represents the input weights from all other neurons
+    '''
     # setup connections for each population individually
     WE21 = 1 # Weight external input to population 1 (E21)
     W122 = 4
@@ -226,7 +250,7 @@ def Weighting(C):
     W225 = 3
     W423 = 0.71
     W526 = 4
-    W622 = X
+    W622 = X #place holder
     W623 = X
     W624 = X
     Weights =(  [WE21, 0, W122, 0, W123, 0, W124, 0, 0, 0, 0, 0],\
@@ -284,15 +308,16 @@ def TII(Input,Force,Activation):
     return Force,Activation
 
 def TIIS(ExInput, speed, Activation):
-
-    # TypeII Speed neuron
-    # model the behaviour of f muscle generating force[forwardspeed(f)] with f
-    # non spiking leaky&integrate neuron. Tuned for maximum output (1) with
-    # input of 200 Hz
-    # ExInput = spikes from the LAL-output neurons (population2)
-    # f = forwardspeed [Voltage]
-    # A_injection = activation of the "muscles"
-
+    '''
+    TypeII Speed neuron
+    model the behaviour of f muscle generating force[forwardspeed(f)] with f
+    non spiking leaky&integrate neuron. Tuned for maximum output (1) with
+    input of 200 Hz
+    :param ExInput = spikes from the LAL-output neurons (population2)
+    :param speed =
+    :param Activation =
+    :return
+    '''
     # behaviour variables
     # membrane capacitance: excitability of the neuron
     C_m = 5e-9 #[Farad]
