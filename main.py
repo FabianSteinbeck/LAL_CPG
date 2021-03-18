0e73e971ae61fc7c548ae68e8c8d816af999e626
efkag <kagioulis.efstathios@gmail.com>
2021-03-18 11:23:54 +0000

    Add test code template to main

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # This is the Simulation of the LAL-SNN
-import  antworld2 as aw
+import antworld2 as aw
+from utils import load_route_naw
+
 
 def main():
-    pass
+    t = 100  # How long to
+    r = 0.05
+    preproc = {'shape': (180, 50)}
+    agent = aw.Agent()
+    path = 'test_route/route1/'
+    route = load_route_naw(path, route_id=1, imgs=True)
+    nav = None  ## here we need to initilize the LAL-CPG
+    traj, nav = agent.test_nav(route, nav, t=t, r=r, sigma=None, preproc=preproc)
 
 
 if __name__ == "__main__":
